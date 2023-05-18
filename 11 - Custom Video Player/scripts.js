da03c2869e3e6aa530b734bab1ba0d35b1adf9c6
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-05-18 22:52:30 +0400

    Get out elements in scrips.js

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,14 @@
+//Get out elements
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+//Build out functions
+function togglePlay() {
+  const method = video.paused ? video.play() : video.pause();
+}
+//Hook up the event listeners
